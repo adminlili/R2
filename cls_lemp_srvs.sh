2dc982d805a0f0dc427bad29c177e41ae47cf534
root <adminL>
2021-01-07 20:32:16 +0000

    F Edited cls_lemp_srvs.sh.

--- cls_lemp_srvs.sh
+++ cls_lemp_srvs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-yum -y remove mysql-community-server MariaDB-server
+yum -y remove mysql-community-server MariaDB-server nginx php-fpm
 yum erase mysql57-community-server
 
 rm -f /etc/my.cnf
+
 rm -rf /var/log/mysql*
 
 userdel -r nginx_user
